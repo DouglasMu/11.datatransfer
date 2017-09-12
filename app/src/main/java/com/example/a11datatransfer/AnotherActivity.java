5d9ff0839d506f01db3c2c6da62f5b9f4f24f6e4
2014011227 <15650799321@163.com>
2017-09-12 14:12:28 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a11datatransfer/AnotherActivity.java
@@ -0,0 +1,36 @@
+package com.example.a11datatransfer;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import com.example.a11datatransfer.R;
+public class AnotherActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_another);
+
+
+
+        Button button=(Button)findViewById(R.id.buttonReturn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=getIntent();
+                String str=intent.getStringExtra("String");
+                Integer in=intent.getIntExtra("int", 20);
+                intent.putExtra("result","String:"+str+" int:"+in);
+                setResult(0,intent);
+                finish();
+            }
+        });
+        Intent intent = getIntent();
+        String str = intent.getStringExtra("String");
+        Integer in = intent.getIntExtra("int", 20);
+        Toast.makeText(this, str + in, Toast.LENGTH_LONG).show();
+
+    }
+}
---
+++ app/src/main/java/com/example/a11datatransfer/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.a11datatransfer;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import com.example.a11datatransfer.R;
+public class MainActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button=(Button)findViewById(R.id.buttonTest);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,AnotherActivity.class);
+                intent.putExtra("String","穆天杨");
+                intent.putExtra("int",20);
+                startActivityForResult(intent,0);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==0 && resultCode==0){
+            String str=data.getStringExtra("result");
+            Toast.makeText(this,str,Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
